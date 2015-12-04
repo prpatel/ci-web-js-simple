fbac7abe696206a1affd4212e4af05847837dcb9
pratik patel <pratik@mypatelspace.com>
2015-12-04 09:56:25 -0500

    fixed bad code style

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Continuous Integration for Web and JS Projects Sample')
+  res.end('Continuous Integration for Web and JS Projects Sample');
 }).listen(3000, '127.0.0.1');
 // broken build
-console.log('Server running at http://127.0.0.1:3000/')
+console.log('Server running at http://127.0.0.1:3000/');
